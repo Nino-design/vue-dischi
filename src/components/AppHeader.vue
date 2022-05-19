a891b508550929c8195b7eb6b2767e1df3ecb9a7
Nino-design <npompili@gmail.com>
2022-05-19 16:30:26 +0200

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="container-fluid">
+  <img src="../img/download.jpg" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+header{
+    background-color: rgba(46,58,70,255);
+}
+
+header img{
+    width: 3%;
+    margin: 1rem 1.5rem;
+};
+
+</style>
